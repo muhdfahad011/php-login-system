6e3635498b0d585cea9271a0277a56e0d67c665f
muhdfahad011 <muhammedfahadjaved0361@gmail.com>
2020-11-21 22:51:26 +0530

    Added config; added login/reg pages; added footer

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php 
+// if there is no constant named __config__, dont load this file
+    if (!defined('__CONFIG__')) {
+        exit('you do not have a config file');
+    }  
+    define("ALLOW FOOTER", true)
+// our CONFIG is below
+?>
